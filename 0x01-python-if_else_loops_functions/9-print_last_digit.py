9303048403bad077eda84779fb4a4c1ff62351c7
honeyvagueness <olaniyanandrew5@gmail.com>
2023-01-04 12:17:14 -0800

    koilk

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
- return print_last_digit(number)
+ drew = number % 10 if number > 10 else number * 0
+ print("{:d}".format(drew), end="")
+ return (drew)
