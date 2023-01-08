a3c74f542e814069d7cb8e45c779df7d0822da4c
honeyvagueness <olaniyanandrew5@gmail.com>
2023-01-08 06:06:30 -0800

    jghjgj

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-def new_in_list(my_list, idx, element):
+
+def new_in_list(my_list, idx, elem):
 
 copy = my_list[:]
+
     if idx < 0 or idx >= len(copy):
+
         return copy
+
     copy[idx] = elem
     return copy
