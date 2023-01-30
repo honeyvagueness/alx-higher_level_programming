d97a8998dba780962819be6793997197df26c099
honeyvagueness <olaniyanandrew5@gmail.com>
2023-01-30 12:54:27 -0800

    h

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -65,13 +65,8 @@ class Rectangle:
                 rect.append("\n")
         return ("".join(rect))
 
-
     def __repr__(self):
-
         """Return the string representation of the Rectangle."""
-
         rect = "Rectangle(" + str(self.__width)
-
         rect += ", " + str(self.__height) + ")"
-
         return (rect)
